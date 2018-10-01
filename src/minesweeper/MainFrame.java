1e2fad20f2068f5a8b4e27609ab74088af0457f1
Jan Majchrzak <jan.majchrzak2001@gmail.com>
2018-10-01 21:20:51 +0200

    Initialized project
    Added MainFrame class

---
+++ src/minesweeper/MainFrame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package minesweeper;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Jan Majchrzak
+ *
+ */
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+
+	}
+
+}
