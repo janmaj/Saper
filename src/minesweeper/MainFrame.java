59baebaa9f7bae412d680654ac1208908923ffa5
Jan Majchrzak <jan.majchrzak2001@gmail.com>
2018-10-08 08:33:12 +0200

    Added simple game funcionality

--- src/minesweeper/MainFrame.java
+++ src/minesweeper/MainFrame.java
@@ -5,11 +5,12 @@ package minesweeper;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.*;
 import javax.swing.border.*;
 
-import sun.security.provider.JavaKeyStore.CaseExactJKS;
+
 
 /**
  * G³ówne okno gry
@@ -27,6 +28,7 @@ public class MainFrame extends JFrame {
 	}
 	
 	private static ImageIcon mineIcon = new ImageIcon(MainFrame.class.getResource("resources/mine.png"));
+	private static ImageIcon plainIcon = new ImageIcon(MainFrame.class.getResource("resources/plain_field.png"));
 	private static ImageIcon flagIcon = new ImageIcon(MainFrame.class.getResource("resources/flag.png"));
 	private static ImageIcon oneIcon = new ImageIcon(MainFrame.class.getResource("resources/1.png"));
 	private static ImageIcon twoIcon = new ImageIcon(MainFrame.class.getResource("resources/2.png"));
@@ -38,9 +40,11 @@ public class MainFrame extends JFrame {
 	private JButton[][] fields;
 	private JCheckBox flagCheckbox;
 	private int bombsLeft = 10;
+	private int flaggedBombs = 0;
 	private boolean gameOver = false;
 	private int width = 9;
 	private int height = 9;
+	private int bombCount = 10;
 	private JButton bombCountDisplay;
 	
 	public MainFrame() {
@@ -53,6 +57,7 @@ public class MainFrame extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		JMenu gameMenu = new JMenu("Gra");
 		JMenuItem restartGameMenuItem = new JMenuItem("Nowa Gra");
+		restartGameMenuItem.addActionListener(event->initialize());
 		JMenuItem exitMenuItem = new JMenuItem("Wyjœcie");
 		exitMenuItem.addActionListener(event->{
 			int result = JOptionPane.showConfirmDialog(this, "Czy na pewno chcesz wyjœæ?", "Chcesz wyjœæ?", JOptionPane.YES_NO_CANCEL_OPTION);
@@ -102,6 +107,37 @@ public class MainFrame extends JFrame {
 				gamePanel.add(fields[j][i]);
 			}
 		}
+		
+		/**
+		 * Ustawienie bomb w losowych polach
+		 */
+		Random random = new Random();
+		for(int i = 0; i<bombCount; i++) {
+			int p = random.nextInt(width);
+			int q = random.nextInt(height);
+			if((boolean)fields[p][q].getAction().getValue("bomb"))
+				i--;
+			else fields[p][q].getAction().putValue("bomb", true);
+		}
+		
+		int[] adjacentIndexes = {1,-1, 1, 0, 1, 1, 0, -1, 0, 1, -1, -1, -1, 0, -1, 1};
+		
+		/**
+		 * W tej pêtli sprawdzana jest iloœæ bomb otaczaj¹cych ka¿de pole
+		 */
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j<width; j++) {
+				int surroundingBombs = 0;
+				for(int k = 0; k<adjacentIndexes.length-1; k+=2) {
+					try {
+						if((boolean)fields[i+adjacentIndexes[k]][j+adjacentIndexes[k+1]].getAction().getValue("bomb"))
+							surroundingBombs++;
+					}catch (ArrayIndexOutOfBoundsException e) {	}
+					fields[i][j].getAction().putValue("surroundingBombs", surroundingBombs);
+				}
+			}
+		}
 		gamePanel.setPreferredSize(new Dimension(30*width, 25*height));
 		pack();
 		setResizable(false);
@@ -112,8 +148,20 @@ public class MainFrame extends JFrame {
 		for(JButton[] column : fields) {
 			for(JButton field : column) {
 				field.getAction().putValue("visible", true);
+				((FieldClickedAction)field.getAction()).revealIcon();
 			}
 		}
+	}
+	
+	private void solved() {
+		gameOver = true;
+		for(JButton[] column : fields) {
+			for(JButton field : column) {
+				field.getAction().putValue("visible", true);
+				((FieldClickedAction)field.getAction()).revealIcon();
+			}
+		}
+		JOptionPane.showMessageDialog(this, "Gratulacje! Uda³o ci sie wygraæ", "Wygrana", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	/**
@@ -144,13 +192,25 @@ public class MainFrame extends JFrame {
 					putValue("flag", true);
 					putValue(SMALL_ICON, flagIcon);
 					bombsLeft--;
+					if((boolean)getValue("bomb"))
+						flaggedBombs++;
+					if(flaggedBombs==bombCount)
+						solved();
 				}
 				bombCountDisplay.setText("" + bombsLeft);
+			}
+			else {
+				if((boolean)getValue("flag"))
+					return;
+				if((boolean)getValue("bomb"))
+					mineClicked();
+				else 
+					revealSurroundingFields();
 			}
 		}
 		
 		public void revealIcon() {
-			if(getValue("bomb").equals("true"))
+			if((boolean)getValue("bomb"))
 				putValue(SMALL_ICON, mineIcon);
 			else {
 				switch ((int)getValue("surroundingBombs")) {
@@ -160,8 +220,36 @@ public class MainFrame extends JFrame {
 					break;
 				case 3: putValue(SMALL_ICON, threeIcon);
 				break;
-				default: putValue(SMALL_ICON, null);
+				default: putValue(SMALL_ICON, plainIcon);
 					break;
+				}
+			}
+		}
+		
+		public void revealSurroundingFields() {
+			int[] adjacentIndexes = {1,-1, 1, 0, 1, 1, 0, -1, 0, 1, -1, -1, -1, 0, -1, 1};
+			if((boolean)getValue("visible"))
+				return;
+			
+			putValue("visible", true);
+			revealIcon();
+			if((int)getValue("surroundingBombs")==0) {
+				int k=0, l=0;
+				for(int i = 0; i < height; i++)
+				{
+					for(int j = 0; j<width; j++) {
+						if(fields[i][j].getAction()==this) {
+							k = i;
+							l = j;
+						}
+					}
+				}
+				
+				for(int i = 0; i<adjacentIndexes.length-1; i+=2) {
+					try {
+						if(!(boolean)fields[k+adjacentIndexes[i]][l+adjacentIndexes[i+1]].getAction().getValue("flag"))
+							((FieldClickedAction)fields[k+adjacentIndexes[i]][l+adjacentIndexes[i+1]].getAction()).revealSurroundingFields();
+					}catch (ArrayIndexOutOfBoundsException e) {	}
 				}
 			}
 		}
